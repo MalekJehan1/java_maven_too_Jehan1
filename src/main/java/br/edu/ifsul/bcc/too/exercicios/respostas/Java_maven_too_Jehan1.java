b5345dd26e33aa2966d890675c7d8159604684a7
20221PF.CC0024 <20221PF.CC0024@pc-155965.passofundo.ifsul.local>
2023-08-07 11:41:02 -0300

    Started Project 07/08/2023

---
+++ src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_Jehan1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.bcc.too.exercicios.respostas;
+
+/**
+ *
+ * @author 20221PF.CC0024
+ */
+public class Java_maven_too_Jehan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
